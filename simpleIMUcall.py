dcb7c07e795886493e31ec308483ac2d0eee8b0f
Vickjoe <55033116+vickjoeobi@users.noreply.github.com>
2022-10-16 16:36:44 +0200

    Create simpleIMUcall.py

---
+++ simpleIMUcall.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+HOST = "localhost"
+PORT = 4223
+UID = "XXYYZZ" # Change XXYYZZ to the UID of your IMU Brick 2.0
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.brick_imu_v2 import BrickIMUV2
+
+if __name__ == "__main__":
+    ipcon = IPConnection() # Create IP connection
+    imu = BrickIMUV2(UID, ipcon) # Create device object
+
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
+
+    # Get current quaternion
+    w, x, y, z = imu.get_quaternion()
+
+    print("Quaternion [W]: " + str(w/16383.0))
+    print("Quaternion [X]: " + str(x/16383.0))
+    print("Quaternion [Y]: " + str(y/16383.0))
+    print("Quaternion [Z]: " + str(z/16383.0))
+
+    input("Press key to exit\n") # Use raw_input() in Python 2
+    ipcon.disconnect()
