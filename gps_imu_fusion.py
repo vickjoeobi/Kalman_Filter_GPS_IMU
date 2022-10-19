6f822ca862282d804f1328f4233d260fecdcdc67
Vickjoe <55033116+vickjoeobi@users.noreply.github.com>
2022-10-19 20:09:03 +0000

    Update

--- gps_imu_fusion.py
+++ gps_imu_fusion.py
@@ -1,79 +1,300 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-HOST = "localhost"
-PORT = 4223
-UID = "XXYYZZ" # Change XXYYZZ to the UID of your IMU Brick 2.0
-GPS_UID = "XYZ" # Change XYZ to the UID of your GPS Bricklet 3.0
+from curses.ascii import alt
+import time
+import sys
+import csv
+from datetime import date
+from turtle import speed
 
 from tinkerforge.ip_connection import IPConnection
-from tinkerforge.brick_imu_v2 import BrickIMUV2
 from tinkerforge.bricklet_gps_v3 import BrickletGPSV3
+from tinkerforge.brick_imu_v2 import BrickIMUV2
+
 import numpy as np
-import math
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from sympy import Symbol, symbols, Matrix, sin, cos
+from sympy import init_printing
+init_printing(use_latex=True)
 
 
-# Callback function for all data callback
-def cb_all_data(acceleration, angular_velocity, euler_angle, quaternion, calibration_status):
-    print("Acceleration [X]: " + str(acceleration[0]/100.0) + " m/s²")
-    print("Acceleration [Y]: " + str(acceleration[1]/100.0) + " m/s²")
-    print("Acceleration [Z]: " + str(acceleration[2]/100.0) + " m/s²")
-    print("Angular Velocity [X]: " + str(angular_velocity[0]/16.0) + " °/s")
-    print("Angular Velocity [Y]: " + str(angular_velocity[1]/16.0) + " °/s")
-    print("Angular Velocity [Z]: " + str(angular_velocity[2]/16.0) + " °/s")
-    print("Euler Angle [Heading]: " + str(euler_angle[0]/16.0) + " °")
-    print("Euler Angle [Roll]: " + str(euler_angle[1]/16.0) + " °")
-    print("Euler Angle [Pitch]: " + str(euler_angle[2]/16.0) + " °")
-    print("Quaternion [W]: " + str(quaternion[0]/16383.0))
-    print("Quaternion [X]: " + str(quaternion[1]/16383.0))
-    print("Quaternion [Y]: " + str(quaternion[2]/16383.0))
-    print("Quaternion [Z]: " + str(quaternion[3]/16383.0))
-    print("Calibration Status: " + format(calibration_status, "08b"))
-    print("")
+def kalman_filter(yawrate, speed, course, latitude, longitude, altitude):
 
-def cb_coordinates(latitude, ns, longitude, ew):
-    print("Latitude: " + str(latitude/1000000.0) + " °")
-    print("N/S: " + ns)
-    print("Longitude: " + str(longitude/1000000.0) + " °")
-    print("E/W: " + ew)
-    print("")
+    numstates = 5  # Number of states
 
-    #Convert the longitude and latitude to x,y,z coordinates
-    lat = latitude/1000000.0
-    lon = longitude/1000000.0
-    r = 6371.0
+    vs, psis, dts, xs, ys, lats, lons = symbols('v \psi T x y lat lon') # Symbols for states
 
-    x = r * math.cos(lat) * math.cos(lon)
-    y = r * math.cos(lat) * math.sin(lon)
-    z = r * math.sin(lat)
+    #We have different frequencies for the IMU and GPS
+    dt = 1.0/50.0 # Sample Rate of the Measurements is 50Hz
+    dtGPS=1.0/10.0 # Sample Rate of GPS is 10Hz
 
-    print("X: " + str(x) + " m")
-    print("Y: " + str(y) + " m")
-    print("Z: " + str(z) + " m")
-    print("")
+    vs, psis, dpsis, dts, xs, ys, lats, lons = symbols('v \psi \dot\psi T x y lat lon')
 
+    # Dynamic model calculates how the states change over time
+    gs = Matrix([[xs+(vs/dpsis)*(sin(psis+dpsis*dts)-sin(psis))],
+                [ys+(vs/dpsis)*(-cos(psis+dpsis*dts)+cos(psis))],
+                [psis+dpsis*dts],
+                [vs],
+                [dpsis]])
+
+    state = Matrix([xs,ys,psis,vs,dpsis])
+
+    # Initial Uncertainty in the states (P0)
+
+    P = np.diag([1000.0, 1000.0, 1000.0, 1000.0, 1000.0])
+
+    # Process Noise Covariance Matrix (Q)
+
+    sGPS     = 0.5*8.8*dt**2  # assume 8.8m/s2 as maximum acceleration, forcing the vehicle
+    sCourse  = 0.1*dt # assume 0.1rad/s as maximum turn rate for the vehicle
+    sVelocity= 8.8*dt # assume 8.8m/s2 as maximum acceleration, forcing the vehicle
+    sYaw     = 1.0*dt # assume 1.0rad/s2 as the maximum turn rate acceleration for the vehicle
+
+    Q = np.diag([sGPS**2, sGPS**2, sCourse**2, sVelocity**2, sYaw**2])
+
+    yawrate = yawrate
+    speed = speed
+    course = course
+    latitude = latitude
+    longitude = longitude
+    altitude = altitude
+
+
+    #Measurement function
+    hs = Matrix([[xs],
+                [ys],
+                [vs],
+                [dpsis]])
+
+    #calculate the Jacobian of the measurement model
+    JHs=hs.jacobian(state)
+
+    # Measurement Noise Covariance Matrix (R)
+
+    varGPS = 6.0 # Standard Deviation of GPS Measurement
+    varspeed = 1.0 # Variance of the speed measurement
+    varyaw = 0.1 # Variance of the yawrate measurement
+    R = np.matrix([[varGPS**2, 0.0, 0.0, 0.0],
+                [0.0, varGPS**2, 0.0, 0.0],
+                [0.0, 0.0, varspeed**2, 0.0],
+                [0.0, 0.0, 0.0, varyaw**2]])
+
+    # Identity Matrix
+    I = np.eye(numstates)
+
+    # Approximate lon/lat to meters conversion to check location
+    RadiusEarth = 6378388.0 # m
+    arc = 2.0*np.pi*(RadiusEarth+altitude)/360.0 # m/°
+
+    dx = arc * np.cos(latitude*np.pi/180.0) * np.hstack((0.0, np.diff(longitude))) # in m
+    dy = arc * np.hstack((0.0, np.diff(latitude))) # in m
+
+    mx = np.cumsum(dx)
+    my = np.cumsum(dy)
+
+    ds = np.sqrt(dx**2+dy**2)
+
+    GPS = (ds!=0.0).astype('bool') # GPS Trigger for Kalman Filter
+
+    # Initial State
+    x = np.matrix([[mx[0], my[0], course[0]/180.0*np.pi, speed[0]/3.6+0.001, yawrate[0]/180.0*np.pi]]).T
+
+    U = float(np.cos(x[2])*x[3])
+    V = float(np.sin(x[2])*x[3])
+
+    # Measurement Vector can also be denoted as Z
+    measurements = np.vstack((mx, my, speed/3.6, yawrate/180.0*np.pi))
+
+    # Preallocation for Plotting
+    x0 = []
+    x1 = []
+    x2 = []
+    x3 = []
+    x4 = []
+    x5 = []
+    Zx = []
+    Zy = []
+    Px = []
+    Py = []
+    Pdx= []
+    Pdy= []
+    Pddx=[]
+    Pddy=[]
+    Kx = []
+    Ky = []
+    Kdx= []
+    Kdy= []
+    Kddx=[]
+    dstate=[]
+
+
+    def savestates(x, Z, P, K):
+        x0.append(float(x[0]))
+        x1.append(float(x[1]))
+        x2.append(float(x[2]))
+        x3.append(float(x[3]))
+        x4.append(float(x[4]))
+        Zx.append(float(Z[0]))
+        Zy.append(float(Z[1]))    
+        Px.append(float(P[0,0]))
+        Py.append(float(P[1,1]))
+        Pdx.append(float(P[2,2]))
+        Pdy.append(float(P[3,3]))
+        Pddx.append(float(P[4,4]))
+        Kx.append(float(K[0,0]))
+        Ky.append(float(K[1,0]))
+        Kdx.append(float(K[2,0]))
+        Kdy.append(float(K[3,0]))
+        Kddx.append(float(K[4,0]))
+
+    while True:
+
+        # Time Update (Prediction)
+        # ========================
+        # Project the state ahead
+        # see "Dynamic Matrix"
+        if np.abs(yawrate)<0.0001: # Driving straight
+            x[0] = x[0] + x[3]*dt * np.cos(x[2])
+            x[1] = x[1] + x[3]*dt * np.sin(x[2])
+            x[2] = x[2]
+            x[3] = x[3]
+            x[4] = 0.0000001 # avoid numerical issues in Jacobians
+            dstate.append(0)
+        else: # otherwise
+            x[0] = x[0] + (x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2]))
+            x[1] = x[1] + (x[3]/x[4]) * (-np.cos(x[4]*dt+x[2])+ np.cos(x[2]))
+            x[2] = (x[2] + x[4]*dt + np.pi) % (2.0*np.pi) - np.pi
+            x[3] = x[3]
+            x[4] = x[4]
+            dstate.append(1)
+        
+        # Calculate the Jacobian of the Dynamic Matrix A
+        # see "Calculate the Jacobian of the Dynamic Matrix with respect to the state vector"
+        a13 = float((x[3]/x[4]) * (np.cos(x[4]*dt+x[2]) - np.cos(x[2])))
+        a14 = float((1.0/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+        a15 = float((dt*x[3]/x[4])*np.cos(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+        a23 = float((x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+        a24 = float((1.0/x[4]) * (-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
+        a25 = float((dt*x[3]/x[4])*np.sin(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
+        JA = np.matrix([[1.0, 0.0, a13, a14, a15],
+                        [0.0, 1.0, a23, a24, a25],
+                        [0.0, 0.0, 1.0, 0.0, dt],
+                        [0.0, 0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0]])
+        
+        
+        # Project the error covariance ahead
+        P = JA*P*JA.T + Q
+        
+        # Measurement Update (Correction)
+        # ===============================
+        # Measurement Function
+        hx = np.matrix([[float(x[0])],
+                        [float(x[1])],
+                        [float(x[3])],
+                        [float(x[4])]])
+
+        if GPS: # with 10Hz, every 5th step
+            JH = np.matrix([[1.0, 0.0, 0.0, 0.0, 0.0],
+                            [0.0, 1.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 0.0, 0.0, 1.0]])
+        else: # every other step
+            JH = np.matrix([[0.0, 0.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 0.0, 0.0],
+                            [0.0, 0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 0.0, 0.0, 1.0]])
+        
+        S = JH*P*JH.T + R
+        K = (P*JH.T) * np.linalg.inv(S)
+
+        # Update the estimate via
+        Z = measurements.reshape(JH.shape[0],1)
+        y = Z - (hx)                         # Innovation or Residual
+        x = x + (K*y)
+
+        # Update the error covariance
+        P = (I - (K*JH))*P
+
+
+        
+        # Save states for Plotting
+        savestates(x, Z, P, K)
+        
+        print()
+        print("x = ")
+        print(x)
+        print("P = ")
+        print(P)
+        print("K = ")
+        print(K)
+        print("Z = ")
+        print(Z)
+        print("hx = ")
+        print(hx)
+        print("y = ")
+        print(y)
+        print("S = ")
+        print(S)
+        print("JA = ")
+        print(JA)
+        print("JH = ")
+        print(JH)
+        print("dstate = ")
+        print(dstate)
+
+        # Predict next state with the most current state and covariance matrix
+        GPS = not GPS
+
+def cb_enumerate(uid, connected_uid, position, hardware_version, firmware_version,
+                 device_identifier, enumeration_type):
+    if enumeration_type == IPConnection.ENUMERATION_TYPE_DISCONNECTED:
+        return
+
+    # Set imu_uid if any IMU is discovered, we assume that there is only 
+    if device_identifier == 18:
+        global imu_uid
+        imu_uid = uid
 
 if __name__ == "__main__":
     ipcon = IPConnection() # Create IP connection
-    imu = BrickIMUV2(UID, ipcon) # Create device object
-    gps = BrickletGPSV3(GPS_UID, ipcon) # Create device object
-
     ipcon.connect(HOST, PORT) # Connect to brickd
-    # Don't use device before ipcon is connected
 
-    # Register all data callback to function cb_all_data
-    imu.register_callback(imu.CALLBACK_ALL_DATA, cb_all_data)
+    # Register Enumerate Callback
+    ipcon.register_callback(IPConnection.CALLBACK_ENUMERATE, cb_enumerate)
 
-    # Register coordinates callback to function cb_coordinates
-    gps.register_callback(gps.CALLBACK_COORDINATES, cb_coordinates)
+    # Trigger Enumerate
+    ipcon.enumerate()
 
-    # Set period for all data callback to 0.1s (100ms)
-    imu.set_all_data_period(100)
+    # Wait for 1 second enumeration
+    time.sleep(1)
 
-    # Set period for coordinates callback to 1s (1000ms)
-    # Note: The coordinates callback is only called every second
-    #       if the coordinates has changed since the last call!
-    gps.set_coordinates_callback_period(1000)
+    gps = BrickletGPSV3(GPS_UID, ipcon) # Create device object
+    imu = BrickIMUV2(IMU_UID, ipcon) # Create device object
+
+
+
+    while True:
+        # Read imu data
+        imu_data = imu.get_all_data()
+        # Read gps data
+        gps_data = gps.get_coordinates()
+
+        yawrate = imu_data.angular_velocity[2]/16.0
+        course = gps_data.heading/100.0
+        speed = gps_data.speed/1000.0
+        latitude = gps_data.latitude/10000000.0
+        longitude = gps_data.longitude/10000000.0
+        altitude = gps_data.altitude/1000.0
+
+        kalman_filter(yawrate, course, speed, latitude, longitude, altitude)
+
+
+
+        # Wait for 100 ms
+        time.sleep(0.1)
+
+
 
     input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
---
+++ kalmanFilter.py
@@ -0,0 +1,364 @@
+import numpy as np
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from sympy import Symbol, symbols, Matrix, sin, cos
+from sympy import init_printing
+init_printing(use_latex=True)
+
+
+numstates = 5  # Number of states
+
+vs, psis, dts, xs, ys, lats, lons = symbols('v \psi T x y lat lon') # Symbols for states
+
+#We have different frequencies for the IMU and GPS
+dt = 1.0/50.0 # Sample Rate of the Measurements is 50Hz
+dtGPS=1.0/10.0 # Sample Rate of GPS is 10Hz
+
+vs, psis, dpsis, dts, xs, ys, lats, lons = symbols('v \psi \dot\psi T x y lat lon')
+
+# Dynamic model calculates how the states change over time
+gs = Matrix([[xs+(vs/dpsis)*(sin(psis+dpsis*dts)-sin(psis))],
+             [ys+(vs/dpsis)*(-cos(psis+dpsis*dts)+cos(psis))],
+             [psis+dpsis*dts],
+             [vs],
+             [dpsis]])
+
+state = Matrix([xs,ys,psis,vs,dpsis])
+
+#calculate the Jacobian of the dynamic model
+print("Jacobian of the dynamic model")
+print(gs.jacobian(state))
+
+# Initial Uncertainty in the states (P0)
+
+P = np.diag([1000.0, 1000.0, 1000.0, 1000.0, 1000.0])
+print(P, P.shape)
+
+# Process Noise Covariance Matrix (Q)
+
+sGPS     = 0.5*8.8*dt**2  # assume 8.8m/s2 as maximum acceleration, forcing the vehicle
+sCourse  = 0.1*dt # assume 0.1rad/s as maximum turn rate for the vehicle
+sVelocity= 8.8*dt # assume 8.8m/s2 as maximum acceleration, forcing the vehicle
+sYaw     = 1.0*dt # assume 1.0rad/s2 as the maximum turn rate acceleration for the vehicle
+
+Q = np.diag([sGPS**2, sGPS**2, sCourse**2, sVelocity**2, sYaw**2])
+print(Q, Q.shape)
+
+
+datafile = '2014-03-26-000-Data.csv'
+
+
+yawrate, speed, course, latitude, longitude, altitude = np.loadtxt(datafile, delimiter=',', unpack=True, 
+                  skiprows=1)
+
+print('\n Read \'%s\' successfully.' % datafile)
+
+#Measurement function
+hs = Matrix([[xs],
+             [ys],
+             [vs],
+             [dpsis]])
+
+#calculate the Jacobian of the measurement model
+JHs=hs.jacobian(state)
+
+# Measurement Noise Covariance Matrix (R)
+
+varGPS = 6.0 # Standard Deviation of GPS Measurement
+varspeed = 1.0 # Variance of the speed measurement
+varyaw = 0.1 # Variance of the yawrate measurement
+R = np.matrix([[varGPS**2, 0.0, 0.0, 0.0],
+               [0.0, varGPS**2, 0.0, 0.0],
+               [0.0, 0.0, varspeed**2, 0.0],
+               [0.0, 0.0, 0.0, varyaw**2]])
+
+# Identity Matrix
+I = np.eye(numstates)
+
+# Approximate lon/lat to meters conversion to check location
+RadiusEarth = 6378388.0 # m
+arc = 2.0*np.pi*(RadiusEarth+altitude)/360.0 # m/°
+
+dx = arc * np.cos(latitude*np.pi/180.0) * np.hstack((0.0, np.diff(longitude))) # in m
+dy = arc * np.hstack((0.0, np.diff(latitude))) # in m
+
+mx = np.cumsum(dx)
+my = np.cumsum(dy)
+
+ds = np.sqrt(dx**2+dy**2)
+
+GPS = (ds!=0.0).astype('bool') # GPS Trigger for Kalman Filter
+
+# Initial State
+x = np.matrix([[mx[0], my[0], course[0]/180.0*np.pi, speed[0]/3.6+0.001, yawrate[0]/180.0*np.pi]]).T
+
+U = float(np.cos(x[2])*x[3])
+V = float(np.sin(x[2])*x[3])
+
+# Measurement Vector can also be denoted as Z
+measurements = np.vstack((mx, my, speed/3.6, yawrate/180.0*np.pi))
+# Lenth of the measurement
+m = measurements.shape[1]
+
+# Preallocation for Plotting
+x0 = []
+x1 = []
+x2 = []
+x3 = []
+x4 = []
+x5 = []
+Zx = []
+Zy = []
+Px = []
+Py = []
+Pdx= []
+Pdy= []
+Pddx=[]
+Pddy=[]
+Kx = []
+Ky = []
+Kdx= []
+Kdy= []
+Kddx=[]
+dstate=[]
+
+
+def savestates(x, Z, P, K):
+    x0.append(float(x[0]))
+    x1.append(float(x[1]))
+    x2.append(float(x[2]))
+    x3.append(float(x[3]))
+    x4.append(float(x[4]))
+    Zx.append(float(Z[0]))
+    Zy.append(float(Z[1]))    
+    Px.append(float(P[0,0]))
+    Py.append(float(P[1,1]))
+    Pdx.append(float(P[2,2]))
+    Pdy.append(float(P[3,3]))
+    Pddx.append(float(P[4,4]))
+    Kx.append(float(K[0,0]))
+    Ky.append(float(K[1,0]))
+    Kdx.append(float(K[2,0]))
+    Kdy.append(float(K[3,0]))
+    Kddx.append(float(K[4,0]))
+
+for filterstep in range(m):
+
+    # Time Update (Prediction)
+    # ========================
+    # Project the state ahead
+    # see "Dynamic Matrix"
+    if np.abs(yawrate[filterstep])<0.0001: # Driving straight
+        x[0] = x[0] + x[3]*dt * np.cos(x[2])
+        x[1] = x[1] + x[3]*dt * np.sin(x[2])
+        x[2] = x[2]
+        x[3] = x[3]
+        x[4] = 0.0000001 # avoid numerical issues in Jacobians
+        dstate.append(0)
+    else: # otherwise
+        x[0] = x[0] + (x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2]))
+        x[1] = x[1] + (x[3]/x[4]) * (-np.cos(x[4]*dt+x[2])+ np.cos(x[2]))
+        x[2] = (x[2] + x[4]*dt + np.pi) % (2.0*np.pi) - np.pi
+        x[3] = x[3]
+        x[4] = x[4]
+        dstate.append(1)
+    
+    # Calculate the Jacobian of the Dynamic Matrix A
+    # see "Calculate the Jacobian of the Dynamic Matrix with respect to the state vector"
+    a13 = float((x[3]/x[4]) * (np.cos(x[4]*dt+x[2]) - np.cos(x[2])))
+    a14 = float((1.0/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+    a15 = float((dt*x[3]/x[4])*np.cos(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+    a23 = float((x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+    a24 = float((1.0/x[4]) * (-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
+    a25 = float((dt*x[3]/x[4])*np.sin(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
+    JA = np.matrix([[1.0, 0.0, a13, a14, a15],
+                    [0.0, 1.0, a23, a24, a25],
+                    [0.0, 0.0, 1.0, 0.0, dt],
+                    [0.0, 0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 1.0]])
+    
+    
+    # Project the error covariance ahead
+    P = JA*P*JA.T + Q
+    
+    # Measurement Update (Correction)
+    # ===============================
+    # Measurement Function
+    hx = np.matrix([[float(x[0])],
+                    [float(x[1])],
+                    [float(x[3])],
+                    [float(x[4])]])
+
+    if GPS[filterstep]: # with 10Hz, every 5th step
+        JH = np.matrix([[1.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 1.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0]])
+    else: # every other step
+        JH = np.matrix([[0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0]])
+    
+    S = JH*P*JH.T + R
+    K = (P*JH.T) * np.linalg.inv(S)
+
+    # Update the estimate via
+    Z = measurements[:,filterstep].reshape(JH.shape[0],1)
+    y = Z - (hx)                         # Innovation or Residual
+    x = x + (K*y)
+
+    # Update the error covariance
+    P = (I - (K*JH))*P
+
+
+    
+    # Save states for Plotting
+    savestates(x, Z, P, K)
+
+
+# Plot the filter performance
+def plotP():
+    fig = plt.figure(figsize=(16,9))
+    plt.semilogy(range(m),Px, label='$x$')
+    plt.step(range(m),Py, label='$y$')
+    plt.step(range(m),Pdx, label='$\psi$')
+    plt.step(range(m),Pdy, label='$v$')
+    plt.step(range(m),Pddx, label='$\dot \psi$')
+
+    plt.xlabel('Filter Step')
+    plt.ylabel('')
+    plt.title('Uncertainty (Elements from Matrix $P$)')
+    plt.legend(loc='best',prop={'size':22})
+    #save the figure
+    fig.savefig('uncertainty.png', bbox_inches='tight')
+
+plotP()
+
+#Kalman gain
+fig = plt.figure(figsize=(16,9))
+plt.step(range(len(measurements[0])),Kx, label='$x$')
+plt.step(range(len(measurements[0])),Ky, label='$y$')
+plt.step(range(len(measurements[0])),Kdx, label='$\psi$')
+plt.step(range(len(measurements[0])),Kdy, label='$v$')
+plt.step(range(len(measurements[0])),Kddx, label='$\dot \psi$')
+
+
+plt.xlabel('Filter Step')
+plt.ylabel('')
+plt.title('Kalman Gain (the lower, the more the measurement fullfill the prediction)')
+plt.legend(prop={'size':18})
+plt.ylim([-0.1,0.1]);
+#save the figure
+fig.savefig('kalman_gain.png', bbox_inches='tight')
+
+#state vector
+def plotx():
+    fig = plt.figure(figsize=(16,16))
+
+    plt.subplot(411)
+    plt.step(range(len(measurements[0])),x0-mx[0], label='$x$')
+    plt.step(range(len(measurements[0])),x1-my[0], label='$y$')
+
+    plt.title('Extended Kalman Filter State Estimates (State Vector $x$)')
+    plt.legend(loc='best',prop={'size':22})
+    plt.ylabel('Position (relative to start) [m]')
+
+    plt.subplot(412)
+    plt.step(range(len(measurements[0])),x2, label='$\psi$')
+    plt.step(range(len(measurements[0])),(course/180.0*np.pi+np.pi)%(2.0*np.pi) - np.pi, label='$\psi$ (from GPS as reference)')
+    plt.ylabel('Course')
+    plt.legend(loc='best',prop={'size':16})
+
+    plt.subplot(413)
+    plt.step(range(len(measurements[0])),x3, label='$v$')
+    plt.step(range(len(measurements[0])),speed/3.6, label='$v$ (from GPS as reference)')
+    plt.ylabel('Velocity')
+    plt.ylim([0, 30])
+    plt.legend(loc='best',prop={'size':16})
+
+    plt.subplot(414)
+    plt.step(range(len(measurements[0])),x4, label='$\dot \psi$')
+    plt.step(range(len(measurements[0])),yawrate/180.0*np.pi, label='$\dot \psi$ (from IMU as reference)')
+    plt.ylabel('Yaw Rate')
+    plt.ylim([-0.6, 0.6])
+    plt.legend(loc='best',prop={'size':16})
+    plt.xlabel('Filter Step')
+
+    plt.savefig('Extended-Kalman-Filter-CTRV-State-Estimates.png', dpi=72, transparent=True, bbox_inches='tight')
+
+plotx()
+
+
+def plotxy():
+
+    fig = plt.figure(figsize=(16,9))
+
+    # EKF State
+    plt.quiver(x0,x1,np.cos(x2), np.sin(x2), color='#94C600', units='xy', width=0.05, scale=0.5)
+    plt.plot(x0,x1, label='EKF Position', c='k', lw=5)
+
+    # Measurements
+    plt.scatter(mx[::5],my[::5], s=50, label='GPS Measurements', marker='+')
+    #cbar=plt.colorbar(ticks=np.arange(20))
+    #cbar.ax.set_ylabel(u'EPE', rotation=270)
+    #cbar.ax.set_xlabel(u'm')
+
+    # Start/Goal
+    plt.scatter(x0[0],x1[0], s=60, label='Start', c='g')
+    plt.scatter(x0[-1],x1[-1], s=60, label='Goal', c='r')
+
+    plt.xlabel('X [m]')
+    plt.ylabel('Y [m]')
+    plt.title('Position')
+    plt.legend(loc='best')
+    plt.axis('equal')
+    plt.tight_layout()
+
+    plt.savefig('Extended-Kalman-Filter-CTRV-Position.png', dpi=72, transparent=True, bbox_inches='tight')
+
+plotxy()
+
+def plotxydetails():
+    fig = plt.figure(figsize=(12,9))
+
+    plt.subplot(221)
+    # EKF State
+    #plt.quiver(x0,x1,np.cos(x2), np.sin(x2), color='#94C600', units='xy', width=0.05, scale=0.5)
+    plt.plot(x0,x1, label='EKF Position', c='g', lw=5)
+
+    # Measurements
+    plt.scatter(mx[::5],my[::5], s=50, label='GPS Measurements', alpha=0.5, marker='+')
+    #cbar=plt.colorbar(ticks=np.arange(20))
+    #cbar.ax.set_ylabel(u'EPE', rotation=270)
+    #cbar.ax.set_xlabel(u'm')
+
+    plt.xlabel('X [m]')
+    plt.xlim(70, 130)
+    plt.ylabel('Y [m]')
+    plt.ylim(140, 200)
+    plt.title('Position')
+    plt.legend(loc='best')
+
+
+    plt.subplot(222)
+
+    # EKF State
+    #plt.quiver(x0,x1,np.cos(x2), np.sin(x2), color='#94C600', units='xy', width=0.05, scale=0.5)
+    plt.plot(x0,x1, label='EKF Position', c='g', lw=5)
+
+    # Measurements
+    plt.scatter(mx[::5],my[::5], s=50, label='GPS Measurements', alpha=0.5, marker='+')
+    #cbar=plt.colorbar(ticks=np.arange(20))
+    #cbar.ax.set_ylabel(u'EPE', rotation=270)
+    #cbar.ax.set_xlabel(u'm')
+
+    plt.xlabel('X [m]')
+    plt.xlim(160, 260)
+    plt.ylabel('Y [m]')
+    plt.ylim(110, 160)
+    plt.title('Position')
+    plt.legend(loc='best')
+    #save fig with white background
+    fig.savefig('Extended-Kalman-Filter-CTRV-Position-Details.png', bbox_inches='tight')
